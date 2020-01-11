1a41ce3563607c1fe0442b005530c9358b36c3aa
Tilak Bisht <tilak_bisht@icloud.com>
2020-01-12 01:22:40 +0530

    added local settings in settings.py file

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -152,3 +152,8 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'abc@gmail.com'
 EMAIL_HOST_PASSWORD = 'xyzpass'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
